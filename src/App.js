2fff57a852b2289f9badc865e53a5d81b2a99d52
Sudeep Balagopal <60956438+Sudeep732000@users.noreply.github.com>
2021-02-21 12:37:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import {React} from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Banner from './Banner';
 import Row from './Row';
 import requests from './request';
-import Banner from './Banner';
-import Navbar from './Navbar';
-
+import Login from './Login';
+import { BrowserRouter as Router, Switch, Route,} from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{user},dispatch] = useStateValue();
+  
   return (
     <div className="app">
-      <Navbar />
-      <Banner />
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title="Documentaris" fetchUrl={requests.fetchDocumentaries}/>
-
+      <Router>
+        {!user ? (
+          <Login />
+        ):(
+          <Switch>
+          <Route path="/">
+            <Navbar />
+            <Banner />
+            <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+          </Route>
+        </Switch>
+        )}
+       
+      </Router> 
     </div>
-  
-  )
-  }
-
+  );
+}
 
 export default App;
